9eae2129ad1c8789b1c4b4b9b8f1950b33fdc6a4
Rayder99 <57237154+Rayder99@users.noreply.github.com>
2020-04-15 19:41:00 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -28,6 +28,8 @@ public class Main extends Application {
 
         try{
         GridPane gp = new GridPane();
+        Label xLabel = new Label("X-Value");
+        Label yLabel = new Label("Y-Value");
         TextField xValue = new TextField("960");
         TextField yValue = new TextField("540");
         Slider xSlider = new Slider(1,1920,1);
@@ -54,7 +56,15 @@ public class Main extends Application {
         TextField clickValue = new TextField("500");
 
         javafx.scene.control.Button startButton = new javafx.scene.control.Button("Auto Click");
-        ProgressBar pb = new ProgressBar(0);
+            javafx.scene.control.Button saveone = new javafx.scene.control.Button("1");
+            javafx.scene.control.Button savetwo = new javafx.scene.control.Button("2");
+            javafx.scene.control.Button savethree = new javafx.scene.control.Button("3");
+            javafx.scene.control.Button savefour = new javafx.scene.control.Button("4");
+            saveone.setMinWidth(180);
+            savetwo.setMinWidth(180);
+            savethree.setMinWidth(180);
+            savefour.setMinWidth(180);
+            ProgressBar pb = new ProgressBar(0);
         pb.setMinWidth(180);
         pb.setMinHeight(40);
 
@@ -65,10 +75,8 @@ public class Main extends Application {
                 int x = (int) xSlider.getValue();
                 int y=  (int) ySlider.getValue();
                 int clicks = Integer.parseInt(clickValue.getText());
+                pb.setProgress(0);
 
-                System.out.println(x);
-                System.out.println(y);
-                System.out.println(clicks);
                 try {
                     clicker(x, y, clicks, pb);
                 } catch (IOException e) {
@@ -80,28 +88,42 @@ public class Main extends Application {
             }
         );
 
-        gp.setConstraints(xSlider,0,0);
-        gp.setConstraints(ySlider,0,2);
-        gp.setConstraints(xValue,0,1);
-        gp.setConstraints(yValue,0,3);
-        gp.setConstraints(clickLabel,0,4);
-        gp.setConstraints(clickValue,0,5);
-        gp.setConstraints(startButton,0,6);
-        gp.setConstraints(pb,0,7);
+        gp.setConstraints(xLabel,0,0);
+        gp.setConstraints(xSlider,0,1);
+        gp.setConstraints(ySlider,0,4);
+        gp.setConstraints(yLabel,0,3);
+        gp.setConstraints(xValue,0,2);
+        gp.setConstraints(yValue,0,5);
+        gp.setConstraints(clickLabel,0,6);
+        gp.setConstraints(clickValue,0,7);
+        gp.setConstraints(startButton,0,8);
+            gp.setConstraints(saveone,0,10);
+            gp.setConstraints(savetwo,0,11);
+            gp.setConstraints(savethree,0,12);
+            gp.setConstraints(savefour,0,13);
+        gp.setConstraints(pb,0,15);
 
+        gp.getChildren().add(xLabel);
         gp.getChildren().add(xSlider);
         gp.getChildren().add(xValue);
+        gp.getChildren().add(yLabel);
         gp.getChildren().add(yValue);
         gp.getChildren().add(ySlider);
         gp.getChildren().add(clickLabel);
         gp.getChildren().add(clickValue);
         gp.getChildren().add(startButton);
         gp.getChildren().add(pb);
+        gp.getChildren().add(saveone);
+        gp.getChildren().add(savetwo);
+        gp.getChildren().add(savethree);
+        gp.getChildren().add(savefour);
         gp.setPadding(new Insets(10,10,10,10));
         gp.setVgap(10);
         gp.setHgap(10);
-        Scene scene = new Scene(gp,200,300) ;
+
+        Scene scene = new Scene(gp,200,490) ;
         primaryStage.setScene(scene);
+        primaryStage.setAlwaysOnTop(true);
         primaryStage.show();} catch (Exception e) {
             e.printStackTrace();
         }
@@ -117,16 +139,16 @@ public class Main extends Application {
         int tick = (int) Math.ceil(quantity/100);
         int progress = 0;
         Robot robot = new Robot();
-        robot.mouseMove(1150, 390);
+        robot.mouseMove(x, y);
         robot.delay(500);
 
         for (int i = 0; i < quantity; i++) {
 
             if (i % tick < 1){
-            //System.out.println(i + " " + tick);
+
                 progress++;
                 pb.setProgress(progress/100);
-                //System.out.println(progress);
+
             }
             robot.mousePress(InputEvent.BUTTON1_MASK);//Linksklick (Alles auswählen)
             robot.mouseRelease(InputEvent.BUTTON1_MASK);
