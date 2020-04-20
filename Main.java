4de24fd7051dce5c43b63a0e6fd103b4cf482e58
Rayder99 <57237154+Rayder99@users.noreply.github.com>
2020-04-20 19:20:53 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,18 +5,16 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.InputEvent;
 import java.io.IOException;
@@ -31,11 +29,28 @@ public class Main extends Application {
         Label xLabel = new Label("X-Value");
         Label yLabel = new Label("Y-Value");
         TextField xValue = new TextField("960");
+
         TextField yValue = new TextField("540");
         Slider xSlider = new Slider(1,1920,1);
         Slider ySlider = new Slider(1,1080,1);
         xSlider.setValue(960);
         ySlider.setValue(540);
+
+        xValue.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+            xSlider.setValue(Integer.parseInt(xValue.getText()));
+            }
+        });
+
+        yValue.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                ySlider.setValue(Integer.parseInt(yValue.getText()));
+            }
+        });
+
+
 
         xSlider.valueProperty().addListener(new ChangeListener<Number>() {
             @Override
@@ -56,15 +71,18 @@ public class Main extends Application {
         TextField clickValue = new TextField("500");
 
         javafx.scene.control.Button startButton = new javafx.scene.control.Button("Auto Click");
-            javafx.scene.control.Button saveone = new javafx.scene.control.Button("1");
-            javafx.scene.control.Button savetwo = new javafx.scene.control.Button("2");
-            javafx.scene.control.Button savethree = new javafx.scene.control.Button("3");
-            javafx.scene.control.Button savefour = new javafx.scene.control.Button("4");
-            saveone.setMinWidth(180);
-            savetwo.setMinWidth(180);
-            savethree.setMinWidth(180);
-            savefour.setMinWidth(180);
-            ProgressBar pb = new ProgressBar(0);
+        javafx.scene.control.Button saveone = new javafx.scene.control.Button("1");
+        javafx.scene.control.Button savetwo = new javafx.scene.control.Button("2");
+        javafx.scene.control.Button savethree = new javafx.scene.control.Button("3");
+        javafx.scene.control.Button savefour = new javafx.scene.control.Button("4");
+        saveone.setMinWidth(180);
+        savetwo.setMinWidth(180);
+        savethree.setMinWidth(180);
+        savefour.setMinWidth(180);
+        ProgressBar pb = new ProgressBar(0);
+
+        Label statusLabel = new Label();
+
         pb.setMinWidth(180);
         pb.setMinHeight(40);
 
@@ -88,6 +106,23 @@ public class Main extends Application {
             }
         );
 
+        saveone.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                xSlider.setValue(1150);
+                ySlider.setValue(370);
+
+            }
+        });
+
+        savetwo.setOnAction((new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                xSlider.setValue(650);
+                ySlider.setValue(750);
+            }
+        }));
+
         gp.setConstraints(xLabel,0,0);
         gp.setConstraints(xSlider,0,1);
         gp.setConstraints(ySlider,0,4);
@@ -97,10 +132,10 @@ public class Main extends Application {
         gp.setConstraints(clickLabel,0,6);
         gp.setConstraints(clickValue,0,7);
         gp.setConstraints(startButton,0,8);
-            gp.setConstraints(saveone,0,10);
-            gp.setConstraints(savetwo,0,11);
-            gp.setConstraints(savethree,0,12);
-            gp.setConstraints(savefour,0,13);
+        gp.setConstraints(saveone,0,10);
+        gp.setConstraints(savetwo,0,11);
+        gp.setConstraints(savethree,0,12);
+        gp.setConstraints(savefour,0,13);
         gp.setConstraints(pb,0,15);
 
         gp.getChildren().add(xLabel);
@@ -121,12 +156,15 @@ public class Main extends Application {
         gp.setVgap(10);
         gp.setHgap(10);
 
-        Scene scene = new Scene(gp,200,490) ;
+        Scene scene = new Scene(gp,200,550) ;
         primaryStage.setScene(scene);
         primaryStage.setAlwaysOnTop(true);
         primaryStage.show();} catch (Exception e) {
             e.printStackTrace();
         }
+
+
+
     }
 
 
@@ -141,19 +179,15 @@ public class Main extends Application {
         Robot robot = new Robot();
         robot.mouseMove(x, y);
         robot.delay(500);
-
         for (int i = 0; i < quantity; i++) {
-
             if (i % tick < 1){
-
+                System.out.println(progress);
                 progress++;
                 pb.setProgress(progress/100);
-
             }
             robot.mousePress(InputEvent.BUTTON1_MASK);//Linksklick (Alles auswählen)
             robot.mouseRelease(InputEvent.BUTTON1_MASK);
             robot.delay(1);
-
         }
         return;
     }
